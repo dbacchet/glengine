5481d2a75fc4fa8d0d4d5e60cdb8c79181436974
Davide Bacchet <davide.bacchet@gmail.com>
2020-09-28 21:46:19 -0700

    tweaks to the dockerfile to use the host X11 with opengl accel (nvidia only for now)

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,12 @@
 CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 docker build ${CURR_DIR} -t ubuntu_glengine
-docker run -it --volume=${CURR_DIR}/../:/code ubuntu_glengine /bin/bash
+
+docker run --gpus all -it \
+    --env=DISPLAY=:0 \
+    --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --name ubuntu_glengine \
+    --rm \
+    -e NVIDIA_DRIVER_CAPABILITIES=graphics \
+    --volume=${CURR_DIR}/../:/code \
+    ubuntu_glengine /bin/bash
