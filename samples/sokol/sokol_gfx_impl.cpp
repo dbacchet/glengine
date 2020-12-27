d291d44653278dacd69985a0bdbb31ba1dab56c7
Davide Bacchet <davide.bacchet@gmail.com>
2020-12-26 23:11:00 -0800

    implementing multiple passes in the toy app

--- samples/sokol/sokol_gfx_impl.cpp
+++ samples/sokol/sokol_gfx_impl.cpp
@@ -2,4 +2,10 @@
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 #define SOKOL_IMPL
+// #define SOKOL_TRACE_HOOKS
 #include "sokol_gfx.h"
+#include "sokol_time.h"
+// imgui support
+#include "imgui/imgui.h"
+#include "sokol_gfx_imgui.h"
+#include "sokol_imgui.h"
